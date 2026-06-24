ca160f8871255c5285709558cbcd24c8a6831d49
Diya Aggarwal <diyaaggarwal8920@gmail.com>
2026-06-24 19:26:49 +0530

    Frontend integrated with backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import axios from "axios";
+
+function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState(null);
+
+  const handleSubmit = async () => {
+    try {
+      const data = input
+        .split("\n")
+        .map(item => item.trim())
+        .filter(item => item);
+
+      const response = await axios.post(
+        "http://localhost:3001/bfhl",
+        { data }
+      );
+
+      setResult(response.data);
+    } catch (error) {
+      console.error(error);
+      alert("API Error");
+    }
+  };
+
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>BFHL Challenge</h1>
+
+      <textarea
+        rows="10"
+        cols="50"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter one edge per line"
+      />
+
+      <br /><br />
+
+      <button onClick={handleSubmit}>
+        Submit
+      </button>
+
+      <hr />
+
+      <pre>
+        {JSON.stringify(result, null, 2)}
+      </pre>
+    </div>
+  );
+}
+
+export default App;
